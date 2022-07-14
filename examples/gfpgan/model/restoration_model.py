20048c8a3fcedfc704ef9029e09fe1204ca70f2a
howespt <howespt@gmail.com>
2022-07-14 14:43:39 -0500

    Add secret handling for s3 upload

--- examples/gfpgan/model/restoration_model.py
+++ examples/gfpgan/model/restoration_model.py
@@ -8,7 +8,7 @@ from basicsr.archs.rrdbnet_arch import RRDBNet
 from gfpgan import GFPGANer
 from PIL import Image
 from realesrgan import RealESRGANer
-from uploader import upload_file
+from utils import upload_file_to_s3
 
 logger = logging.getLogger(__name__)
 
@@ -17,15 +17,12 @@ upscale = 2  # The final upsampling scale of the image
 arch = 'clean'  # The GFPGAN architecture. Option: clean | original
 channel = 2  # Channel multiplier for large networks of StyleGAN2
 
-# TODO: Consider exposing these params to the user.
 aligned = False  # Input are aligned faces
 only_center_face = False  # Only restore the center face
 paste_back = True  # Paste the restored faces back to images
 
 # The model path
-GFPGAN_PATH = (
-    'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth'
-)
+GFPGAN_PATH = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth'
 # The background upsampler
 ESRGAN_PATH = 'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth'
 
@@ -33,6 +30,15 @@ RESIZE_DEFAULT_MAX = 1400
 
 
 class RestorationModel:
+    def __init__(self, **kwargs) -> None:
+        self._config = kwargs['config']
+        self.s3_config = {
+            'aws_access_key_id': self._config['secrets']['gfpgan_aws_access_key_id'],
+            'aws_secret_access_key': self._config['secrets']['gfpgan_aws_secret_access_key'],
+            'aws_region': self._config['secrets']['gfpgan_aws_region'],
+        }
+        self.s3_bucket = self._config['secrets']['gfpgan_aws_bucket']
+
     def load(self):
         self.model = RRDBNet(
             num_in_ch=3,
@@ -72,10 +78,10 @@ class RestorationModel:
                 ) = self.restore_image(image)
                 if restored_img is not None:
                     rgb_image = rotate_axis(restored_img)
-                    url_for_image = upload_image(rgb_image)
+                    url_for_image = upload_image(rgb_image, self.s3_bucket, self.s3_config)
                     restored_images.append(url_for_image)
                 else:
-                    logger.warning("Failed to restore image")
+                    logger.warning(f'Failed to restore image, {instance["image_url"]}')
             except Exception as e:
                 logger.error(e)
         return restored_images
@@ -90,12 +96,12 @@ class RestorationModel:
         return input_img, cropped_faces, restored_faces, restored_img
 
 
-def upload_image(image):
+def upload_image(image, bucket=None, aws_config=None):
     temp_file = tempfile.NamedTemporaryFile(suffix='.png')
     image = Image.fromarray(image)
     image.save(temp_file.name, format='png')
     temp_file.seek(0)
-    return upload_file(temp_file.name)
+    return upload_file_to_s3(temp_file.name, bucket=bucket, aws_config=aws_config)
 
 
 def load_img_from_url(img_url: str):
@@ -103,9 +109,7 @@ def load_img_from_url(img_url: str):
     img_request.raise_for_status()
     image_content = np.asarray(bytearray(img_request.content), dtype='uint8')
     image = cv2.imdecode(image_content, cv2.IMREAD_COLOR)
-    scale = min(
-        RESIZE_DEFAULT_MAX / image.shape[1], RESIZE_DEFAULT_MAX / image.shape[0]
-    )
+    scale = min(RESIZE_DEFAULT_MAX / image.shape[1], RESIZE_DEFAULT_MAX / image.shape[0])
     if scale < 1:
         image = cv2.resize(image, (0, 0), fx=scale, fy=scale)
     return image
