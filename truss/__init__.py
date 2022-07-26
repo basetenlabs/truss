d6e86d6798d3870cfa6e5be75bda87c55479baa3
Bola Malek <malek.bola@gmail.com>
2022-07-26 21:49:27 +0000

    Add version to command line

--- truss/__init__.py
+++ truss/__init__.py
@@ -6,4 +6,9 @@ from single_source import get_version
 
 __version__ = get_version(__name__, Path(__file__).parent.parent)
 
+
+def version():
+    return __version__
+
+
 from truss.build import from_directory, init, kill_all, mk_truss
