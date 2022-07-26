073d10fe8d3598d5da1ad3ad180662dc3fbaaeb7
Bola Malek <malek.bola@gmail.com>
2022-07-26 06:39:21 +0000

    Use Single source for package version

--- truss/__init__.py
+++ truss/__init__.py
@@ -1,5 +1,9 @@
 # flake8: noqa F401
 
-__version__ = "0.0.20"
+from pathlib import Path
+
+from single_source import get_version
+
+__version__ = get_version(__name__, Path(__file__).parent.parent)
 
 from truss.build import from_directory, init, kill_all, mk_truss
