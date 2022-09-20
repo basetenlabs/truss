0c9ed31b644c771b56f362c599835c1a8c79a01d
Pankaj Gupta <pankaj.roark@gmail.com>
2022-09-20 17:22:57 +0000

    Add truss docker

---
+++ control/truss_docker/server/common/util.py
@@ -0,0 +1,21 @@
+def model_supports_predict_proba(model):
+    if not hasattr(model, "predict_proba"):
+        return False
+    if hasattr(
+        model, "_check_proba"
+    ):  # noqa eg Support Vector Machines *can* predict proba if they made certain choices while training
+        try:
+            model._check_proba()
+            return True
+        except AttributeError:
+            return False
+    return True
+
+
+def assign_request_to_inputs_instances_after_validation(request):
+    # we will treat "instances" and "inputs" the same
+    if "instances" in request and "inputs" not in request:
+        request["inputs"] = request["instances"]
+    elif "inputs" in request and "instances" not in request:
+        request["instances"] = request["inputs"]
+    return request
