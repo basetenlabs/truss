6b021f635c4ee4a66f42eddb29817d3bcc73d415
Pankaj Gupta <pankaj.roark@gmail.com>
2022-09-28 22:49:48 +0000

    Add doc comment

--- truss/patch/types.py
+++ truss/patch/types.py
@@ -4,6 +4,15 @@ from typing import Dict
 
 @dataclass
 class TrussSignature:
+    """Truss signature stores information for calculating patches for future
+    changes to Truss.
+
+    Currently, it stores hashes of all of the paths in the truss directory, and
+    the truss config contents. Path hashes allow calculating added/updated/removes
+    paths in future trusses compared to this. Config contents allow calculating
+    config changes, such as add/update/remove of python requirements etc.
+    """
+
     content_hashes_by_path: Dict[str, str]
     config: str
 
