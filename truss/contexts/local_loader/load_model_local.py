5d538fe88f86c7a86f1da4f69000ff640854466f
Pankaj Gupta <pankaj.roark@gmail.com>
2022-12-14 00:23:28 +0000

    Refactor

--- truss/contexts/local_loader/load_model_local.py
+++ truss/contexts/local_loader/load_model_local.py
@@ -1,11 +1,12 @@
-import copy
 import inspect
 from pathlib import Path
-from typing import Dict
 
 from truss.contexts.local_loader.truss_module_loader import truss_module_loaded
+from truss.contexts.local_loader.utils import (
+    prepare_secrets,
+    signature_accepts_keyword_arg,
+)
 from truss.contexts.truss_context import TrussContext
-from truss.local.local_config_handler import LocalConfigHandler
 from truss.truss_spec import TrussSpec
 
 
@@ -28,33 +29,13 @@ class LoadModelLocal(TrussContext):
             model_class = getattr(module, spec.model_class_name)
             model_class_signature = inspect.signature(model_class)
             model_init_params = {}
-            if _signature_accepts_keyword_arg(model_class_signature, "config"):
+            if signature_accepts_keyword_arg(model_class_signature, "config"):
                 model_init_params["config"] = spec.config.to_dict()
-            if _signature_accepts_keyword_arg(model_class_signature, "data_dir"):
+            if signature_accepts_keyword_arg(model_class_signature, "data_dir"):
                 model_init_params["data_dir"] = truss_dir / "data"
-            if _signature_accepts_keyword_arg(model_class_signature, "secrets"):
-                model_init_params["secrets"] = _prepare_secrets(spec)
+            if signature_accepts_keyword_arg(model_class_signature, "secrets"):
+                model_init_params["secrets"] = prepare_secrets(spec)
             model = model_class(**model_init_params)
             if hasattr(model, "load"):
                 model.load()
             return model
-
-
-def _signature_accepts_keyword_arg(signature: inspect.Signature, kwarg: str) -> bool:
-    return kwarg in signature.parameters or _signature_accepts_kwargs(signature)
-
-
-def _signature_accepts_kwargs(signature: inspect.Signature) -> bool:
-    for param in signature.parameters.values():
-        if param.kind == inspect.Parameter.VAR_KEYWORD:
-            return True
-    return False
-
-
-def _prepare_secrets(spec: TrussSpec) -> Dict[str, str]:
-    secrets = copy.deepcopy(spec.secrets)
-    local_secerts = LocalConfigHandler.get_config().secrets
-    for secret_name in secrets:
-        if secret_name in local_secerts:
-            secrets[secret_name] = local_secerts[secret_name]
-    return secrets
