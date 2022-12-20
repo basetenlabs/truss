4bd9dcc2b4426f55db2056465663ea8180cb57d4
Bola Malek <malek.bola@gmail.com>
2022-12-20 17:58:55 +0000

    Remove tmp dir

--- tmpnjstky6x/truss/model/model.py
+++ tmpnjstky6x/truss/model/model.py
@@ -1,7 +1,7 @@
 from typing import Dict, List
 
-import numpy as np
-from tensorflow import keras
+MODEL_BASENAME = "model"
+MODEL_EXTENSIONS = [".joblib", ".pkl", ".pickle"]
 
 
 class Model:
@@ -9,13 +9,21 @@ class Model:
         self._data_dir = kwargs["data_dir"]
         config = kwargs["config"]
         model_metadata = config["model_metadata"]
+        # LightGBM does not implement a `predict_proba` function
+        self._supports_predict_proba = False
         self._model_binary_dir = model_metadata["model_binary_dir"]
         self._model = None
 
     def load(self):
-        self._model = keras.models.load_model(
-            str(self._data_dir / self._model_binary_dir)
-        )
+        import joblib
+
+        model_binary_dir_path = self._data_dir / self._model_binary_dir
+        paths = [
+            (model_binary_dir_path / MODEL_BASENAME).with_suffix(model_extension)
+            for model_extension in MODEL_EXTENSIONS
+        ]
+        model_file_path = next(path for path in paths if path.exists())
+        self._model = joblib.load(model_file_path)
 
     def preprocess(self, request: Dict) -> Dict:
         """
@@ -34,7 +42,8 @@ class Model:
     def predict(self, request: Dict) -> Dict[str, List]:
         response = {}
         inputs = request["inputs"]
-        inputs = np.array(inputs)
-        result = self._model.predict(inputs).tolist()
+        result = self._model.predict(inputs)
         response["predictions"] = result
+        if self._supports_predict_proba:
+            response["probabilities"] = self._model.predict_proba(inputs).tolist()
         return response
