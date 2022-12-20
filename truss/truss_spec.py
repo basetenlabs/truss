a9beeede4c8c6298aac3d1d5c4a05122fb7145c4
Bola Malek <malek.bola@gmail.com>
2022-12-20 18:16:07 +0000

    Update mapping

--- truss/truss_spec.py
+++ truss/truss_spec.py
@@ -75,11 +75,12 @@ class TrussSpec:
     @property
     def python_version(self) -> str:
         return self._config.python_version
+
     @property
     def canonical_python_version(self) -> str:
         return {
             "py39": "3.9",
-            "py38": "3.9",
+            "py38": "3.8",
             "py37": "3.7",
         }[self.python_version]
 
