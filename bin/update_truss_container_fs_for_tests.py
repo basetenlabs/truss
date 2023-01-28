30b61ee924ddf21004454cd37f2885a1adef6163
Pankaj Gupta <pankaj.roark@gmail.com>
2023-01-28 15:09:25 +0000

    Add script to generate truss_container_fs

--- bin/update_truss_container_fs_for_tests.py
+++ bin/update_truss_container_fs_for_tests.py
@@ -1,1 +1,44 @@
 #!/usr/bin/env python3
+
+# This script generates truss/test_data/truss_container_fs from
+# truss/test_data/test_truss.
+#
+# Idea is to get access to representative /app folder content of a sample truss
+# for testing purposes.
+#
+# Contents of truss_container_fs should then be checked into git.
+
+import subprocess
+from pathlib import Path
+
+ROOT = str(Path(__file__).parent.parent.resolve())
+
+
+if __name__ == "__main__":
+    subprocess.run(["truss", "run-image", "truss/test_data/test_truss"], cwd=ROOT)
+    ps_output = subprocess.check_output(
+        [
+            "docker",
+            "ps",
+            "--filter",
+            "label=truss_dir=truss/test_data/test_truss",
+            "--format",
+            "'{{.Names}}'",
+        ]
+    )
+    container_name = ps_output.decode("utf-8").strip()[1:-1]
+    subprocess.run(
+        [
+            "docker",
+            "cp",
+            f"{container_name}:/app",
+            "truss/test_data/truss_container_fs",
+        ]
+    )
+    subprocess.run(
+        [
+            "docker",
+            "kill",
+            container_name,
+        ]
+    )
