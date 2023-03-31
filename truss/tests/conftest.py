78e80b7ab68404fbb1c44b9c17e23fdd6885e334
Bola Malek <bola@baseten.co>
2023-03-31 01:29:22 -0400

    Revert "Clean up a little"
    This reverts commit 4ff79b582f847e8a5407f5b5e2da003c674e04d0.

--- truss/tests/conftest.py
+++ truss/tests/conftest.py
@@ -18,7 +18,6 @@ from tensorflow.keras import layers
 from tensorflow.keras.layers.experimental import preprocessing
 from transformers import AutoModelWithLMHead, AutoTokenizer, pipeline
 from truss.build import create, init
-from truss.constants import MODEL_DOCKERFILE_NAME
 from truss.contexts.image_builder.serving_image_builder import ServingImageBuilder
 from truss.truss_config import DEFAULT_BUNDLED_PACKAGES_DIR
 from truss.types import Example
