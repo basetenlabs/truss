81794168a2cfbac57a56b276542ffdf485cd70ac
Pankaj Gupta <pankaj.roark@gmail.com>
2023-04-20 06:26:44 +0000

    Add b10cp

--- truss/templates/server/common/external_data_resolver.py
+++ truss/templates/server/common/external_data_resolver.py
@@ -1,9 +1,11 @@
 import shutil
+import subprocess
 from pathlib import Path
 
 import requests
 
 BLOB_DOWNLOAD_TIMEOUT_SECS = 600  # 10 minutes
+B10CP_PATH = "/app/bin/b10cp"
 
 
 def download_external_data(data_dir: Path, config: dict):
@@ -17,6 +19,13 @@ def download_external_data(data_dir: Path, config: dict):
 
 
 def _download(URL: str, download_to: Path):
+    download_to.parent.mkdir(exist_ok=True, parents=True)
+
+    b10cp_success = _try_b10cp(URL, download_to)
+    if b10cp_success:
+        return
+
+    # Fallback if b10cp can't handle the url
     # Streaming download to keep memory usage low
     resp = requests.get(
         URL,
@@ -25,6 +34,22 @@ def _download(URL: str, download_to: Path):
         timeout=BLOB_DOWNLOAD_TIMEOUT_SECS,
     )
     resp.raise_for_status()
-    download_to.parent.mkdir(exist_ok=True, parents=True)
     with download_to.open("wb") as file:
         shutil.copyfileobj(resp.raw, file)
+
+
+def _try_b10cp(URL: str, download_to: Path) -> bool:
+    if not Path(B10CP_PATH).exists():
+        return False
+
+    proc = subprocess.run(
+        [
+            B10CP_PATH,
+            "-source",
+            URL,  # Add quotes to work with any special characters.
+            "-target",
+            str(download_to),
+        ],
+        check=False,
+    )
+    return proc.returncode == 0
