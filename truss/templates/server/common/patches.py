35839c35a8c12e5dc587e5eafe1c767b409f336c
Abu Qader <48742992+aspctu@users.noreply.github.com>
2023-05-22 21:04:11 +0000

    adding whisper patch

--- truss/templates/server/common/patches.py
+++ truss/templates/server/common/patches.py
@@ -1,15 +1,70 @@
 import logging
 import os
+from typing import Union
 
-from huggingface_hub.utils import EntryNotFoundError
 from joblib import Parallel, delayed
-from requests.exceptions import HTTPError
-from transformers.utils import hub
-from transformers.utils.hub import HUGGINGFACE_CO_RESOLVE_ENDPOINT, cached_file
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+def _download(url: str, root: str, in_memory: bool) -> Union[bytes, str]:
+    import hashlib
+    import urllib
+    import warnings
+
+    from tqdm import tqdm
+
+    os.makedirs(root, exist_ok=True)
+
+    expected_sha256 = url.split("/")[-2]
+    download_target = os.path.join(root, os.path.basename(url))
+
+    if os.path.exists(download_target) and not os.path.isfile(download_target):
+        raise RuntimeError(f"{download_target} exists and is not a regular file")
+
+    if os.path.isfile(download_target):
+        with open(download_target, "rb") as f:
+            model_bytes = f.read()
+        if hashlib.sha256(model_bytes).hexdigest() == expected_sha256:
+            return model_bytes if in_memory else download_target
+        else:
+            warnings.warn(
+                f"{download_target} exists, but the SHA256 checksum does not match; re-downloading the file"
+            )
+
+    with urllib.request.urlopen(url) as source, open(download_target, "wb") as output:
+        with tqdm(
+            total=int(source.info().get("Content-Length"))
+            if source.info().get("Content-Length") is not None
+            else None,
+            ncols=80,
+            unit="iB",
+            unit_scale=True,
+            unit_divisor=1024,
+        ) as loop:
+            while True:
+                buffer = source.read(8192)
+                if not buffer:
+                    break
+
+                output.write(buffer)
+                loop.update(len(buffer))
+
+    model_bytes = open(download_target, "rb").read()
+    if hashlib.sha256(model_bytes).hexdigest() != expected_sha256:
+        raise RuntimeError(
+            "Model has been downloaded but the SHA256 checksum does not not match. Please retry loading the model."
+        )
+
+    return model_bytes if in_memory else download_target
+
+
+def whisper_patch():
+    import whisper
+
+    whisper._download = _download
 
 
 def get_checkpoint_shard_files(
@@ -27,6 +82,10 @@ def get_checkpoint_shard_files(
     _commit_hash=None,
 ):
     import json
+
+    from huggingface_hub.utils import EntryNotFoundError
+    from requests.exceptions import HTTPError
+    from transformers.utils.hub import HUGGINGFACE_CO_RESOLVE_ENDPOINT, cached_file
 
     if not os.path.isfile(index_filename):
         raise ValueError(
@@ -97,6 +156,8 @@ def huggingface_patch():
     """
     Patching Huggingface downloads to make them parallelized.
     """
+    from transformers.utils import hub
+
     hub.get_checkpoint_shard_files = get_checkpoint_shard_files
 
 
@@ -105,7 +166,10 @@ def apply_patches(enabled: bool, requirements: list):
     Apply patches to certain functions. The patches are contained in the PATCHES list.
     If a patch cannot be applied, it logs the name of the function and the exception details.
     """
-    PATCHES = {"transformers": huggingface_patch}
+    PATCHES = {
+        "transformers": huggingface_patch,
+        "whisper": whisper_patch,
+    }
     if not enabled:
         return
     for requirement in requirements:
