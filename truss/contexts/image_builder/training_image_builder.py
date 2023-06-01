e46b509d3ac96b2bcf99d95cec1a938ce38b0f5d
Bola Malek <bola@baseten.co>
2023-06-01 16:47:58 -0700

    Fix tests and drop mulltiple copies

--- truss/contexts/image_builder/training_image_builder.py
+++ truss/contexts/image_builder/training_image_builder.py
@@ -107,7 +107,6 @@ class TrainingImageBuilder(ImageBuilder):
             tag = truss_base_image_tag(
                 python_version=python_version,
                 use_gpu=config.resources.use_gpu,
-                live_reload=config.live_reload,
                 version_tag=TRUSS_BASE_IMAGE_VERSION_TAG,
             )
             base_image_name_and_tag = f"{base_image_name}:{tag}"
@@ -115,7 +114,6 @@ class TrainingImageBuilder(ImageBuilder):
             base_image_name_and_tag=base_image_name_and_tag,
             config=self._spec.config,
             python_version=python_version,
-            live_reload=config.live_reload,
             bundled_packages_dir_exists=bundled_packages_dir_exists,
             should_install_system_requirements=should_install_system_requirements,
             should_install_requirements=should_install_requirements,
