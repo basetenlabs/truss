1b70bd1a0dc6caf3ff2a378f8c474eee250d873a
Bola Malek <bola@baseten.co>
2023-06-03 07:14:43 -0700

    Split up files for readability and testing

--- truss/contexts/local_loader/local_server_loader.py
+++ truss/contexts/local_loader/local_server_loader.py
@@ -3,17 +3,12 @@ import subprocess
 import sys
 import venv
 from pathlib import Path
-from threading import Thread
-from typing import Dict, List, Optional
+from typing import Optional
 
-import dockerfile
 from truss.contexts.image_builder.serving_image_builder import ServingImageBuilder
-from truss.util.path import (
-    build_truss_target_directory,
-    copy_file_path,
-    copy_tree_or_file,
-)
-from watchfiles import Change, watch
+from truss.contexts.local_loader.docker_build_emulator import DockerBuildEmulator
+from truss.contexts.local_loader.truss_file_watcher import TrussFilesWatcher
+from truss.util.path import build_truss_target_directory
 
 
 class EnvBuilder(venv.EnvBuilder):
@@ -23,62 +18,6 @@ class EnvBuilder(venv.EnvBuilder):
 
     def post_setup(self, context):
         self.context = context
-
-
-class DockerBuildEmulator:
-    def __init__(self, dockerfile_path: Path) -> None:
-        self.commands = dockerfile.parse_file(str(dockerfile_path))
-        self.env_vars: Dict[str, str] = {}
-        self.entry_point: List[str] = []
-        self.work_dir: Path = Path("/")
-
-    def _resolve_env(self, key: str) -> str:
-        if key.startswith("$"):
-            key = key.replace("$", "", 1)
-            v = self.env_vars[key]
-            return v
-        return key
-
-    def _resolve_values(self, keys: List[str]) -> List[str]:
-        return list(map(self._resolve_env, keys))
-
-    def run(self, context_dir: Path, fs_root_dir: Path):
-        for cmd in self.commands:
-            values = self._resolve_values(cmd.value)
-            if cmd.cmd == "ENV":
-                self.env_vars[values[0]] = values[1]
-            if cmd.cmd == "ENTRYPOINT":
-                self.entry_point = list(values)
-            if cmd.cmd == "COPY":
-                # symlink to path
-                src, dst = values
-                src = src.replace("./", "", 1)
-                dst = dst.replace("/", "", 1)
-                copy_tree_or_file(context_dir / src, fs_root_dir / dst)
-            if cmd.cmd == "WORKDIR":
-                self.work_dir = self.work_dir / values[0]
-
-
-class TrussFilesWatcher(Thread):
-    def __init__(self, watch_path: Path, mirror_path: Path) -> None:
-        super().__init__(daemon=True)
-        self.watch_path = watch_path
-        self.mirror_path = mirror_path
-
-    def run(self):
-        for changes in watch(str(self.watch_path)):
-            for change in changes:
-                op, path = change
-                rel_path = Path(path).relative_to(self.watch_path.resolve())
-                if op == Change.modified:
-                    copy_file_path(
-                        self.watch_path / rel_path,
-                        self.mirror_path / rel_path,
-                    )
-
-    def stop(self):
-        self._stop()
-        self.join()
 
 
 class LocalServerLoader:
@@ -159,8 +98,7 @@ class LocalServerLoader:
             "--port",
             str(self.port),
         ]
-        t = TrussFilesWatcher(self.truss_path, venv_dir / "app/")
-        t.start()
+        TrussFilesWatcher(self.truss_path, venv_dir / "app/").start()
         execution_env_vars = {
             **execution_env_vars,
             "SETUP_JSON_LOGGER": "False",
---
+++ truss/contexts/local_loader/truss_file_watcher.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from threading import Thread
+
+from truss.util.path import copy_file_path
+from watchfiles import Change, watch
+
+
+class TrussFilesWatcher(Thread):
+    def __init__(self, watch_path: Path, mirror_path: Path) -> None:
+        super().__init__(daemon=True)
+        self.watch_path = watch_path
+        self.mirror_path = mirror_path
+
+    def run(self):
+        for changes in watch(str(self.watch_path)):
+            for change in changes:
+                op, path = change
+                rel_path = Path(path).relative_to(self.watch_path.resolve())
+                if op == Change.modified:
+                    copy_file_path(
+                        self.watch_path / rel_path,
+                        self.mirror_path / rel_path,
+                    )
