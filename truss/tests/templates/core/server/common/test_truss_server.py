c98fc4c618eb06b6d9abbef142ebf5085c409e03
Pankaj Gupta <pankaj.roark@gmail.com>
2023-06-18 21:49:50 +0000

    chkpt

--- truss/tests/templates/core/server/common/test_truss_server.py
+++ truss/tests/templates/core/server/common/test_truss_server.py
@@ -1,5 +1,6 @@
 import os
 import signal
+import socket
 import sys
 import tempfile
 import time
@@ -10,6 +11,8 @@ import yaml
 
 
 def test_truss_server_termination(truss_container_fs):
+    port = 10123
+
     def start_truss_server(stdout_capture_file_path):
         sys.stdout = open(stdout_capture_file_path, "w")
         app_path = truss_container_fs / "app"
@@ -18,7 +21,7 @@ def test_truss_server_termination(truss_container_fs):
         from common.truss_server import TrussServer
 
         config = yaml.safe_load((app_path / "config.yaml").read_text())
-        server = TrussServer(http_port=10123, config=config)
+        server = TrussServer(http_port=port, config=config)
         server.start()
 
     stdout_capture_file = tempfile.NamedTemporaryFile()
@@ -30,3 +33,15 @@ def test_truss_server_termination(truss_container_fs):
     time.sleep(2.0)
     print(Path(stdout_capture_file.name).read_text())
     assert not subproc.is_alive()
+    assert _is_port_available(port)
+
+
+def _is_port_available(port):
+    try:
+        # Try to bind to the given port
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(("localhost", port))
+            return True
+    except socket.error:
+        # Port is already in use
+        return False
