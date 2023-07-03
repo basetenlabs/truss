052d2b4207b582ccefd8abdc044a8386842c700d
Varun Shenoy <vnshenoy@stanford.edu>
2023-07-03 11:08:42 -0700

    Revert "Revert "Simplify config file to be more user friendly (#407)" (#414)"
    This reverts commit 0ef5c1273cd324ada1e33d8738d7c98afad5bca1.

--- truss/build.py
+++ truss/build.py
@@ -232,10 +232,11 @@ def init(
                           Truss in. The directory is created if it doesn't exist.
     """
     config = TrussConfig(
-        model_type="custom",
-        model_framework=ModelFrameworkType.CUSTOM,
         python_version=map_to_supported_python_version(infer_python_version()),
     )
+
+    if trainable:
+        config.train.resources.use_gpu = True
 
     target_directory_path = populate_target_directory(
         config=config, target_directory_path=target_directory
@@ -343,6 +344,7 @@ def _populate_default_training_code(
     # now we don't support this.
     truss_template = "custom"
     template_path = TEMPLATES_DIR / truss_template
+
     truss_training_module_dir = target_directory_path / config.train.training_module_dir
     copy_tree_path(template_path / "train", truss_training_module_dir)
 
