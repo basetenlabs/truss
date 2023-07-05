cf3f683d1ad25e6b8d281902efb0715922f17ce4
Pankaj Gupta <pankaj.roark@gmail.com>
2023-07-05 23:33:04 +0000

    move test to separate file

--- truss/tests/templates/core/server/common/test_truss_server.py
+++ truss/tests/templates/core/server/common/test_truss_server.py
@@ -7,7 +7,6 @@ import time
 from multiprocessing import Process
 from pathlib import Path
 
-import psutil
 import pytest
 import yaml
 
@@ -43,47 +42,6 @@ def test_truss_server_termination(truss_container_fs):
     assert _is_port_available(port)
 
 
-@pytest.mark.integration
-def test_truss_control_server_termination(truss_control_container_fs):
-    ctrl_port = 10123
-    inf_port = 10124
-
-    def start_truss_server(stdout_capture_file_path):
-        sys.stdout = open(stdout_capture_file_path, "w")
-        app_path = truss_control_container_fs / "app"
-        sys.path.append(str(app_path))
-        control_path = truss_control_container_fs / "control" / "control"
-        sys.path.append(str(control_path))
-
-        from server import ControlServer
-
-        control_server = ControlServer(
-            python_executable_path=sys.executable,
-            inf_serv_home=str(app_path),
-            control_server_port=ctrl_port,
-            inference_server_port=inf_port,
-        )
-        control_server.run()
-
-    stdout_capture_file = tempfile.NamedTemporaryFile()
-    subproc = Process(target=start_truss_server, args=(stdout_capture_file.name,))
-    subproc.start()
-    proc_id = subproc.pid
-    try:
-        time.sleep(2.0)
-        # Port should have been taken up by truss server
-        assert not _is_port_available(ctrl_port)
-        os.kill(proc_id, signal.SIGTERM)
-        subproc.join(timeout=10)
-        # Print on purpose for help with debugging, otherwise hard to know what's going on
-        print(Path(stdout_capture_file.name).read_text())
-        assert not subproc.is_alive()
-        # Port should be free now
-        assert _is_port_available(ctrl_port)
-    finally:
-        _kill_process_tree(proc_id)
-
-
 def _is_port_available(port):
     try:
         # Try to bind to the given port
@@ -93,13 +51,3 @@ def _is_port_available(port):
     except socket.error:
         # Port is already in use
         return False
-
-
-def _kill_process_tree(pid: int):
-    try:
-        proc = psutil.Process(pid)
-        for child_proc in proc.children(recursive=True):
-            child_proc.kill()
-        proc.kill()
-    except psutil.NoSuchProcess:
-        pass
