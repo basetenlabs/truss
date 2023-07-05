b4f7c0b05360c6385e7b5f9fb1314c7fa0339bb4
Varun Shenoy <vnshenoy@stanford.edu>
2023-07-05 21:44:15 +0000

    log parsing works

--- truss/templates/server/common/logging.py
+++ truss/templates/server/common/logging.py
@@ -5,11 +5,44 @@ from pythonjsonlogger import jsonlogger
 
 LEVEL: int = logging.INFO
 
+
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+    """
+    A custom JsonFormatter to reformat the web server log entries.
+    """
+
+    def process_log_record(self, log_record):
+        message = log_record.get("message", "")
+
+        # Parse the necessary information from the message
+        parts = message.split(" ")
+        request_type = parts[2][1:]
+        endpoint = parts[3].split("%3A")[1]
+        status_code = parts[5]
+
+        status_codes = {
+            "200": "OK",
+            "404": "NOT FOUND",
+            "500": "INTERNAL ERROR",
+            "503": "SERVICE UNAVAILABLE"
+            # Add more status codes and their corresponding descriptions if needed
+        }
+
+        new_message = (
+            f"{request_type} /{endpoint} {status_code} {status_codes.get(status_code)}"
+        )
+
+        # Replace 'message' field in the log record
+        log_record["message"] = new_message
+
+        return super().process_log_record(log_record)
+
+
 JSON_LOG_HANDLER = logging.StreamHandler(stream=sys.stdout)
 JSON_LOG_HANDLER.set_name("json_logger_handler")
 JSON_LOG_HANDLER.setLevel(LEVEL)
 JSON_LOG_HANDLER.setFormatter(
-    jsonlogger.JsonFormatter("%(asctime)s %(levelname)s %(message)s")
+    CustomJsonFormatter("%(asctime)s %(levelname)s %(message)s")
 )
 
 
