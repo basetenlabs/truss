615b6a45499d6f514f7852370326070e13be4d30
Varun Shenoy <vnshenoy@stanford.edu>
2023-07-31 14:26:30 -0700

    Add `version.txt` into build to prevent cache migrations (#490)
    * added hf token to dockerfile
    
    * add prints
    
    * move hf token to the part where its needed
    
    * add version.txt
    
    * added version.txt
    
    * remove unnecessary files
    
    * adding files back
    
    * reverting

--- truss/contexts/image_builder/cache_warmer.py
+++ truss/contexts/image_builder/cache_warmer.py
@@ -1,7 +1,16 @@
 if __name__ == "__main__":
+    import os
     import sys
 
     from huggingface_hub import hf_hub_download
+
+    # TODO(varun): might make sense to move this check + write to a separate `prepare_cache.py` script
+    file_path = os.path.expanduser("~/.cache/huggingface/hub/version.txt")
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+    if not os.path.isfile(file_path):
+        with open(file_path, "w") as f:
+            f.write("1")
 
     file_name = sys.argv[1]
     repo_name = sys.argv[2]
