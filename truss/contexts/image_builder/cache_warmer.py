2975bf758534fffea3075d4d6f9edb6f5da80069
Bola Malek <bola@baseten.co>
2023-08-18 15:06:29 -0700

    Revert "Bundling Private Weights from GCP (#552)" (#591)
    This reverts commit 0a52e5209b569d682307bde6e19fcae188f8ab42.

--- truss/contexts/image_builder/cache_warmer.py
+++ truss/contexts/image_builder/cache_warmer.py
@@ -1,41 +1,25 @@
 import sys
 from pathlib import Path
 
-from google.cloud import storage
 from huggingface_hub import hf_hub_download
 
 
-def download_file(
-    repo_name, file_name, revision_name=None, key_file="/app/data/service_account.json"
-):
-    # Check if repo_name starts with "gs://"
-    if "gs://" in repo_name:
-        # Create directory if not exist
-        repo_name = repo_name.replace("gs://", "")
-        cache_dir = Path(f"/app/hf_cache/{repo_name}")
-        cache_dir.mkdir(parents=True, exist_ok=True)
+def download_file(repo_name, file_name, revision_name=None):
+    secret = None
+    secret_path = Path("/etc/secrets/hf_access_token")
 
-        # Connect to GCS storage
-        try:
-            storage_client = storage.Client.from_service_account_json(key_file)
-            bucket = storage_client.bucket(repo_name)
-            blob = bucket.blob(file_name)
-            # Download the blob to a file
-            blob.download_to_filename(f"{cache_dir}/{file_name}")
-        except Exception as e:
-            raise RuntimeError(f"Failure downloading file from GCS: {e}")
-    else:
-        secret_path = Path("/etc/secrets/hf_access_token")
-        secret = secret_path.read_text().strip() if secret_path.exists() else None
-        try:
-            hf_hub_download(repo_name, file_name, revision=revision_name, token=secret)
-        except FileNotFoundError:
-            raise RuntimeError(
-                "Hugging Face repository not found (and no valid secret found for possibly private repository)."
-            )
+    if secret_path.exists():
+        secret = secret_path.read_text().strip()
+    try:
+        hf_hub_download(repo_name, file_name, revision=revision_name, token=secret)
+    except FileNotFoundError:
+        raise RuntimeError(
+            "Hugging Face repository not found (and no valid secret found for possibly private repository)."
+        )
 
 
 if __name__ == "__main__":
+    # TODO(varun): might make sense to move this check + write to a separate `prepare_cache.py` script
     file_path = Path.home() / ".cache/huggingface/hub/version.txt"
     file_path.parent.mkdir(parents=True, exist_ok=True)
 
