e001f1592513fb16057767c872b6814ca5f2b385
Abu Qader <48742992+aspctu@users.noreply.github.com>
2024-02-20 16:16:06 +0000

    updating config spec

--- truss/config/trt_llm.py
+++ truss/config/trt_llm.py
@@ -15,47 +15,67 @@ class TRTLLMModelArchitecture(Enum):
 
 class TRTLLMQuantizationType(Enum):
     NO_QUANT: str = "no_quant"
-    WEIGHTS_ONLY: str = "weights_only"
+    WEIGHTS_ONLY_INT8: str = "weights_int8"
     WEIGHTS_KV_INT8: str = "weights_kv_int8"
+    WEIGHTS_ONLY_INT4: str = "weights_int4"
+    WEIGHTS_KV_INT4: str = "weights_kv_int4"
     SMOOTH_QUANT: str = "smooth_quant"
+    FP8: str = "fp8"
+    FP8_KV: str = "fp8_kv"
 
 
-class TRTLLMBuildConfiguration(BaseModel):
+class TrussTRTLLMPluginConfiguration(BaseModel):
+    multi_block_mode: bool = False
+    paged_kv_cache: bool = False
+    use_fused_mlp: bool = False
+
+
+class TrussTRTLLMBuildConfiguration(BaseModel):
     huggingface_ckpt_repository: str
     base_model_architecture: TRTLLMModelArchitecture
     max_input_len: int
     max_output_len: int
     max_batch_size: int
-    quantization_type: TRTLLMQuantizationType = TRTLLMQuantizationType.NO_QUANT
+    max_beam_width: int
+    max_prompt_embedding_table_size: int
     gather_all_token_logits: bool = False
-    multi_block_mode: bool = False
-    calibrate_kv_cache: bool = False
+    strongly_typed: bool = False
+    quantization_type: TRTLLMQuantizationType = TRTLLMQuantizationType.NO_QUANT
+    tensor_parallel_count: int = 1
+    pipeline_parallel_count: int = 1
+    plugin_configuration: TrussTRTLLMPluginConfiguration = (
+        TrussTRTLLMPluginConfiguration()
+    )
 
 
-class TRTLLMConfiguration(BaseModel):
-    engine_repository: Optional[str]
-    tokenizer_repository: Optional[str]
-    tensor_parallel_count: Optional[int] = 1
-    pipeline_parallel_count: Optional[int] = 1
-    build_configuration: Optional[TRTLLMBuildConfiguration] = None
+class TrussTRTLLMServingConfiguration(BaseModel):
+    engine_repository: str
+    tokenizer_repository: str
+    tensor_parallel_count: int = 1
+    pipeline_parallel_count: int = 1
+
+
+class TrussTRTLLMConfiguration(BaseModel):
+    serve: Optional[TrussTRTLLMServingConfiguration] = None
+    build: Optional[TrussTRTLLMBuildConfiguration] = None
 
     @model_validator(mode="after")
     def check_minimum_required_configuration(self):
-        serve_engine_configuration = (
-            self.engine_repository is not None and self.tokenizer_repository is not None
-        )
-        build_engine_configuration = self.build_configuration is not None
-        if serve_engine_configuration and build_engine_configuration:
-            logger.warning(
-                "Both serve and build configurations are provided. Serve configuration will be used."
-            )
-        if (self.engine_repository is None) ^ (self.tokenizer_repository is None):
-            raise ValueError(
-                "Both engine_repository and tokenizer_repository must be provided"
-            )
+        if not self.serve and not self.build:
+            raise ValueError("Either serve or build configurations must be provided")
+        if self.serve and self.build:
+            raise ValueError("Both serve and build configurations cannot be provided")
+        if self.serve is not None:
+            if (self.serve.engine_repository is None) ^ (
+                self.serve.tokenizer_repository is None
+            ):
+                raise ValueError(
+                    "Both engine_repository and tokenizer_repository must be provided"
+                )
+        return self
 
     @property
     def requires_build(self):
-        if self.engine_repository is None:
+        if self.build is not None:
             return True
         return False
