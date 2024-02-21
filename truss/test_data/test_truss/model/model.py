86374ff75f15f76c77e6941d57f6470d3872dfbf
Bola Malek <bola@baseten.co>
2024-02-21 17:08:05 +0000

    Revert changes to test data

--- truss/test_data/test_truss/model/model.py
+++ truss/test_data/test_truss/model/model.py
@@ -1,8 +1,19 @@
+from typing import Any
+
+
 class Model:
+    def __init__(self, **kwargs) -> None:
+        self._data_dir = kwargs["data_dir"]
+        self._config = kwargs["config"]
+        self._secrets = kwargs["secrets"]
+        self._model = None
+
     def load(self):
-        import time
+        # Load model here and assign to self._model.
+        pass
 
-        time.sleep(3)
-
-    def predict(self, request):
-        return {"prediction": [1]}
+    def predict(self, model_input: Any) -> Any:
+        model_output = {}
+        # Invoke model on model_input and calculate predictions here.
+        model_output["predictions"] = []
+        return model_output
