767fde66519cf57993fe9646ffd1976cfdf2407c
marius.baseten <marius.killinger@baseten.co>
2024-02-28 17:24:36 -0800

    demo

--- example_workflow/workflow.py
+++ example_workflow/workflow.py
@@ -1,23 +1,3 @@
-r"""
-A workflow combines multiple remote operations to produce a final result.
-
-The simplest case is a plain chain:
-
-Preprocssing (CPU) --> ML model (GPU) --> Postprocessing (CPU)
-
-But any DAG-like structure can be realized:
-
-    --> B --->
-  /            \
-A               E --> G
-  \            /       \
-    --> C --> D         --> I
-              \           /
-                F -----> H
-
-Remote operations are conceptually the same as truss models hosted on Baseten.
-"""
-
 import logging
 import random
 import string
@@ -42,7 +22,6 @@ class WorkflowResult(pydantic.BaseModel):
 
 
 class GenerateData(slay.ProcessorBase):
-
     default_config = slay.Config(image=IMAGE_COMMON)
 
     def gen_data(self, params: Parameters) -> str:
@@ -51,73 +30,12 @@ class GenerateData(slay.ProcessorBase):
         )
 
 
-IMAGE_TRANSFORMERS_GPU = (
-    slay.Image()
-    .cuda("12.8")
-    .pip_requirements_txt("common_requirements.txt")
-    .pip_install("transformers")
-)
-
-
-class MistraLLMConfig(pydantic.BaseModel):
-    hf_model_name: str
-
-
-class MistralLLM(slay.ProcessorBase[MistraLLMConfig]):
-
-    default_config = slay.Config(
-        image=IMAGE_TRANSFORMERS_GPU,
-        resources=slay.Resources().cpu(12).gpu("A100"),
-        user_config=MistraLLMConfig(hf_model_name="EleutherAI/mistral-6.7B"),
-    )
-
-    def __init__(
-        self,
-        config: slay.Config = slay.provide_config(),
-    ) -> None:
-        super().__init__(config)
-        try:
-            subprocess.check_output(["nvidia-smi"], text=True)
-        except:
-            raise RuntimeError(
-                f"Cannot run `{self.__class__}`, because host has no CUDA."
-            )
-        import transformers
-
-        model_name = config.user_config.hf_model_name
-        tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
-        model = transformers.AutoModelForCausalLM.from_pretrained(model_name)
-        self._model = transformers.pipeline(
-            "text-generation", model=model, tokenizer=tokenizer
-        )
-
-    def llm_gen(self, data: str) -> str:
-        return self._model(data, max_length=50)
-
-
-class MistralP(Protocol):
-    def __init__(self, config: slay.Config) -> None:
-        ...
-
-    def llm_gen(self, data: str) -> str:
-        ...
-
-
 class TextToNum(slay.ProcessorBase):
     default_config = slay.Config(image=IMAGE_COMMON)
 
-    def __init__(
-        self,
-        config: slay.Config = slay.provide_config(),
-        mistral: MistralP = slay.provide(MistralLLM),
-    ) -> None:
-        super().__init__(config)
-        self._mistral = mistral
-
     def to_num(self, data: str, params: Parameters) -> int:
         number = 0
-        generated_text = self._mistral.llm_gen(data)
-        for char in generated_text:
+        for char in data:
             number += ord(char)
 
         return number
@@ -148,36 +66,7 @@ class Workflow(slay.ProcessorBase):
 
 
 if __name__ == "__main__":
-    log_format = "%(levelname).1s%(asctime)s %(filename)s:%(lineno)d] %(message)s"
-    date_format = "%m%d %H:%M:%S"
-    logging.basicConfig(level=logging.INFO, format=log_format, datefmt=date_format)
+    params = Parameters(length=100, num_partitions=5)
 
-    # Local test or dev execution - context manager makes sure local processors
-    # are instantiated and injected.
-    # with slay.run_local():
-    #     wf = Workflow()
-    #     params = Parameters()
-    #     result = wf.run(params=params)
-    #     print(result)
-
-    class FakeMistralLLM(slay.ProcessorBase):
-        def llm_gen(self, data: str) -> str:
-            return data.upper()
-
-    with slay.run_local():
-        text_to_num = TextToNum(mistral=FakeMistralLLM())
-        wf = Workflow(text_to_num=text_to_num)
-        params = Parameters()
-        result = wf.run(params=params)
-        print(result)
-
-    # Gives a `UsageError`, because not in `run_local` context.
-    try:
-        wf = Workflow()
-    except slay.UsageError as e:
-        print(e)
-
-    # A "marker" to designate which processors should be deployed as public remote
-    # service points. Depenedency processors will also be deployed, but only as
-    # "internal" services, not as a "public" sevice endpoint.
-    slay.deploy_remotely([Workflow])
+    wf = Workflow()
+    print(wf.run(params))
