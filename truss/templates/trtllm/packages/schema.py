70ee70fa6c4ac14500a20a1104cd2458e77c6239
joostinyi <63941848+joostinyi@users.noreply.github.com>
2024-04-30 08:42:49 -0700

    Update TRT-LLM builder truss config interface (#916)
    * change truss trt-llm build interface
    
    * increment versions

--- truss/templates/trtllm/packages/schema.py
+++ truss/templates/trtllm/packages/schema.py
@@ -84,14 +84,15 @@ class ModelInput:
 
 # The following are duplicated from the underlying base image.
 # We list them as a comment for posterity:
+# TODO(@joostinyi): Refactor to benefit from type sharing across serving image and truss lib
 #
-# class TRTLLMModelArchitecture(Enum):
+# class TrussTRTLLMModel(Enum):
 #     LLAMA: str = "llama"
 #     MISTRAL: str = "mistral"
 #     DEEPSEEK: str = "deepseek"
 
 
-# class TRTLLMQuantizationType(Enum):
+# class TrussTRTLLMQuantizationType(str, Enum):
 #     NO_QUANT: str = "no_quant"
 #     WEIGHTS_ONLY_INT8: str = "weights_int8"
 #     WEIGHTS_KV_INT8: str = "weights_kv_int8"
@@ -101,25 +102,50 @@ class ModelInput:
 #     FP8: str = "fp8"
 #     FP8_KV: str = "fp8_kv"
 
+
 # class TrussTRTLLMPluginConfiguration(BaseModel):
 #     multi_block_mode: bool = False
 #     paged_kv_cache: bool = True
 #     use_fused_mlp: bool = False
 
+
+# class CheckpointSource(Enum):
+#     HF: str = "HF"
+#     GCS: str = "GCS"
+#     LOCAL: str = "LOCAL"
+
+
+# class CheckpointRepository(BaseModel):
+#     source: CheckpointSource
+#     repo: str
+
 # class TrussTRTLLMBuildConfiguration(BaseModel):
-#     base_model_architecture: TRTLLMModelArchitecture
+#     base_model: TrussTRTLLMModel
 #     max_input_len: int
 #     max_output_len: int
 #     max_batch_size: int
 #     max_beam_width: int
 #     max_prompt_embedding_table_size: int = 0
-#     huggingface_ckpt_repository: Optional[str]
+#     checkpoint_repository: CheckpointRepository
 #     gather_all_token_logits: bool = False
 #     strongly_typed: bool = False
-#     quantization_type: TRTLLMQuantizationType = TRTLLMQuantizationType.NO_QUANT
+#     quantization_type: TrussTRTLLMQuantizationType = (
+#         TrussTRTLLMQuantizationType.NO_QUANT
+#     )
 #     tensor_parallel_count: int = 1
 #     pipeline_parallel_count: int = 1
-#     plugin_configuration: TrussTRTLLMPluginConfiguration = TrussTRTLLMPluginConfiguration()
+#     plugin_configuration: TrussTRTLLMPluginConfiguration = (
+#         TrussTRTLLMPluginConfiguration()
+#     )
+#     use_fused_mlp: bool = False
+
+#     class Config:
+#         json_encoders = {
+#             TrussTRTLLMModel: lambda x: x.value,
+#             TrussTRTLLMQuantizationType: lambda x: x.value,
+#             CheckpointSource: lambda x: x.value,
+#         }
+
 
 # class TrussTRTLLMServingConfiguration(BaseModel):
 #     engine_repository: str
@@ -127,22 +153,26 @@ class ModelInput:
 #     tensor_parallel_count: int = 1
 #     pipeline_parallel_count: int = 1
 
-# class TrussTRTLLMConfiguration(BaseModel):
+
+# class TRTLLMConfiguration(BaseModel):
 #     serve: Optional[TrussTRTLLMServingConfiguration] = None
 #     build: Optional[TrussTRTLLMBuildConfiguration] = None
 
-#     @model_validator(mode="after")
-#     def check_minimum_required_configuration(self):
+#     def __init__(self, **data):
+#         super().__init__(**data)
+#         self._validate_minimum_required_configuration()
+
+#     # In pydantic v2 this would be `@model_validator(mode="after")` and
+#     # the __init__ override can be removed.
+#     def _validate_minimum_required_configuration(self):
 #         if not self.serve and not self.build:
-#             raise ValueError(
-#                 "Either serve or build configurations must be provided"
-#             )
+#             raise ValueError("Either serve or build configurations must be provided")
 #         if self.serve and self.build:
-#             raise ValueError(
-#                 "Both serve and build configurations cannot be provided"
-#             )
+#             raise ValueError("Both serve and build configurations cannot be provided")
 #         if self.serve is not None:
-#             if (self.serve.engine_repository is None) ^ (self.serve.tokenizer_repository is None):
+#             if (self.serve.engine_repository is None) ^ (
+#                 self.serve.tokenizer_repository is None
+#             ):
 #                 raise ValueError(
 #                     "Both engine_repository and tokenizer_repository must be provided"
 #                 )
@@ -153,3 +183,8 @@ class ModelInput:
 #         if self.build is not None:
 #             return True
 #         return False
+
+#     # TODO(Abu): Replace this with model_dump(json=True)
+#     # when pydantic v2 is used here
+#     def to_json_dict(self):
+#         return json.loads(self.json())
