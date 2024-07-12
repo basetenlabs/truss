b703f8844755fd6a592e89b3a9973352e38146f7
nicolasgere <nicolas.gerelm@gmail.com>
2024-07-12 23:35:53 +0000

    add-otlp-trace

--- truss/templates/server/common/truss_server.py
+++ truss/templates/server/common/truss_server.py
@@ -167,6 +167,7 @@ class BasetenEndpoints:
 
         # calls ModelWrapper.__call__, which runs validate, preprocess, predict, and postprocess
         with tracer.start_as_current_span("predict"):
+            print("HELLO HELLO")
             response: Union[Dict, Generator] = await model(
                 body,
                 headers=utils.transform_keys(request.headers, lambda key: key.lower()),
