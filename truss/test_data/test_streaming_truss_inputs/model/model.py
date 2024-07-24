6dc783e591682cb39c1f5676165e2c8633db29c4
GaryBaseten <gary.boone@baseten.co>
2024-07-24 15:30:34 +0000

    tweaks

---
+++ truss/test_data/test_streaming_truss_inputs/model/model.py
@@ -0,0 +1,24 @@
+from typing import Any, Dict, List
+
+
+class Model:
+    def __init__(self, trt_llm, **kwargs) -> None:
+        self._data_dir = kwargs["data_dir"]
+        self._config = kwargs["config"]
+        self._secrets = kwargs["secrets"]
+        self._model = None
+        self._engine = trt_llm["engine"]
+
+    def load(self):
+        # Load model here and assign to self._model.
+        pass
+
+    def predict(self, model_input: Any) -> Dict[str, List]:
+        def inner():
+            for i in range(5):
+                # Raise error partway through if throw_error is set
+                if i == 3 and model_input.get("throw_error"):
+                    raise Exception("error")
+                yield str(i)
+
+        return inner()
