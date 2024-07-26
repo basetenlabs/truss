a596af47375dba8ec5cdd16332b96297aa1ead43
Gary Boone <gary.boone@baseten.co>
2024-07-26 10:59:43 -0700

    Revert "--amend"
    This reverts commit be5e31447a1f3cc09ee0119d85f58beae2cd3937.

--- truss/templates/trtllm-briton/src/engine.py
+++ truss/templates/trtllm-briton/src/engine.py
@@ -139,7 +139,7 @@ class Engine:
         briton_monitor_thread.start()
         self._loaded = True
 
-    def validate_input(self, prompt, model_input):
+    def validate_input(self, prompt, model_input, request):
         # Input length <= max_input_length.
         logging.info(f" ==> prompt: {prompt}")
         if prompt:
@@ -149,21 +149,20 @@ class Engine:
                     f"Input length `{input_length}` is longer than allowed by max_input_length: {self._max_input_len}."
                 )
 
+        # Beam width == max_beam_width.
+        logging.info(f" ==> request.beam_width: {request.beam_width}")
         beam_width = model_input.get("beam_width", None)
         if beam_width:
-            # Beam width == 1.
-            # There's no need to check if streaming is passed in the input. 
-            # Briton explicitly sets streaming to true in britonToTbRequest().
-            # https://github.com/basetenlabs/baseten/blob/1c2c9cbe1adafc0c736566bd012abbe7d7e2c2da/briton/src/briton.cpp#L272
-            if beam_width != 1:
-                raise ValueError("TensorRT-LLM requires beam_width to equal 1")
-
-            # If Beam width != max_beam_width, TensorRt-LLM will fail an assert.
-            # Since Briton sets streaming, the max_beam_width must aslo equal 1. 
-            if self._max_beam_width != 1:
+            if beam_width != self._max_beam_width:
                 raise ValueError(
-                    "TensorRT-LLM requires max_beam_width to equal 1."
+                    "TensorRT-LLM requires beam_width to equal max_beam_width."
                 )
+
+            # Beam width == 1 for streaming.
+            stream = model_input.get("stream", False) # TODO(Gary): Remove.
+            logging.info(f" ==> stream: {stream}")    # TODO(Gary): Remove.
+            if model_input.get("stream", False) and beam_width != 1:
+                raise ValueError("TensorRT-LLM requires beam_width to equal 1 for streaming")
 
     async def predict(self, model_input):
         """
@@ -210,7 +209,8 @@ class Engine:
                     getattr(request, words).append(word)
 
         logging.info(f" ==> model_input: {model_input}")
-        self.validate_input(prompt, model_input)
+        logging.info(f" ==> request: {request}")
+        self.validate_input(prompt, model_input, request)
 
         resp_iter = self._stub.Infer(request)
 
