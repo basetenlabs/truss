aa35e2e71dd875a39748571757666c3ab39b6fb7
Bryce Dubayah <bdubayah@gmail.com>
2024-08-25 23:02:42 -0700

    BT-11405 json schema structured output (#1095)

--- truss/templates/trtllm-briton/src/engine.py
+++ truss/templates/trtllm-briton/src/engine.py
@@ -1,3 +1,5 @@
+import hashlib
+import json
 import os
 import signal
 import socket
@@ -5,11 +7,15 @@ import subprocess
 import threading
 import time
 from itertools import count
+from pathlib import Path
+from typing import Any, Dict, Optional
 
 import briton_pb2
 import briton_pb2_grpc
 import grpc
 from fastapi import HTTPException
+from outlines.models.transformers import TransformerTokenizer
+from outlines.processors.structured import JSONLogitsProcessor
 from transformers import AutoTokenizer
 from truss.config.trt_llm import TrussTRTLLMBuildConfiguration
 from truss.constants import OPENAI_COMPATIBLE_TAG
@@ -28,6 +34,9 @@ MODEL_INPUT_TO_BRITON_FIELD = {
     "runtime_top_k": "runtime_top_k",
     "runtime_top_p": "runtime_top_p",
 }
+
+# Use a directory that can be picked up by baseten-fs
+FSM_CACHE_DIR = "/cache/model/fsm_cache"
 
 
 def is_port_available(port, host="localhost"):
@@ -98,11 +107,15 @@ class Engine:
         self._tokenizer = AutoTokenizer.from_pretrained(
             self._tokenizer_repository, token=self._hf_token
         )
+
+        self._fsm_cache = FsmCache(Path(FSM_CACHE_DIR), self._tokenizer)
+
         # Start engine
         config_str = f"""
     engine_path: "{self._data_dir.resolve()}"
     hf_tokenizer: "{self._tokenizer_repository}"
     kv_cache_free_gpu_mem_fraction: {self._kv_cache_free_gpu_mem_fraction}
+    fsm_cache_dir: "{FSM_CACHE_DIR}"
 """
         config_pbtxt_path = (self._data_dir / "briton_config.pbtxt").resolve()
         config_pbtxt_path.write_text(config_str)
@@ -176,6 +189,10 @@ class Engine:
         if prompt is None and "messages" in model_input:
             messages = model_input.pop("messages")
             prompt = self._tokenizer.apply_chat_template(messages, tokenize=False)
+        if prompt is None or len(prompt) == 0:
+            raise HTTPException(status_code=400, detail="Prompt cannot be empty.")
+
+        self.validate_input(model_input)
 
         request_id = int(str(os.getpid()) + str(next(self._request_id_counter)))
         request = briton_pb2.InferenceRequest(
@@ -193,13 +210,15 @@ class Engine:
             and self._tokenizer.pad_token_id is not None
         ):
             request.pad_id = self._tokenizer.pad_token_id
+        # Add output schema hash if response_format is provided
+        schema_hash = self._fsm_cache.add_schema_from_input(model_input)
+        if schema_hash is not None:
+            request.output_schema_hash = schema_hash
         set_briton_request_fields_from_model_input(model_input, request)
         for words in ["bad_words", "stop_words"]:
             if words in model_input:
                 for word in model_input[words].split(","):
                     getattr(request, words).append(word)
-
-        self.validate_input(model_input)
 
         resp_iter = self._stub.Infer(request)
 
@@ -222,8 +241,68 @@ class Engine:
             if ex.code() == grpc.StatusCode.INVALID_ARGUMENT:
                 raise HTTPException(status_code=400, detail=ex.details())
         except Exception as ex:
-            print(f"An error has occurred: {ex}")
-            raise ex
+            raise HTTPException(status_code=500, detail=f"An error has occurred: {ex}")
+
+
+class FsmCache:
+    def __init__(self, cache_dir: Path, tokenizer: AutoTokenizer):
+        self._cache_dir = cache_dir
+        if not self._cache_dir.exists():
+            self._cache_dir.mkdir(parents=True, exist_ok=True)
+        self._cache = set(f.name for f in self._cache_dir.iterdir() if f.is_file())
+        self._tokenizer = tokenizer
+
+    def add_schema_from_input(self, model_input: Dict[str, Any]) -> Optional[str]:
+        schema = self._extract_schema(model_input)
+        if schema is None:
+            return None
+        schema_str = json.dumps(schema)
+        schema_hash = hashlib.sha256(schema_str.encode()).hexdigest()
+        if schema_hash not in self._cache:
+            fsm = self._create_fsm(schema_str)
+            (self._cache_dir / schema_hash).write_bytes(fsm.SerializeToString())
+            self._cache.add(schema_hash)
+        return schema_hash
+
+    def _create_fsm(self, schema: str) -> briton_pb2.StatesToTokens:  # type: ignore[name-defined]
+        outlines_tokenizer = TransformerTokenizer(self._tokenizer)
+        logits_processor = JSONLogitsProcessor(schema, outlines_tokenizer)
+        guide = logits_processor.fsm
+
+        states_to_tokens = {}
+        for state, token_to_next_state in guide.states_to_token_maps.items():
+            states_to_tokens[state] = briton_pb2.TokenToNextState(  # type: ignore[attr-defined]
+                token_to_next_state=token_to_next_state
+            )
+        states_to_tokens_pb = briton_pb2.StatesToTokens(  # type: ignore[attr-defined]
+            states_to_tokens=states_to_tokens,
+            vocab_size=len(self._tokenizer.vocab),
+            eos_token_id=self._tokenizer.eos_token_id,
+        )
+        return states_to_tokens_pb
+
+    @staticmethod
+    def _extract_schema(model_input: Dict[str, Any]) -> Optional[Dict[str, Any]]:
+        if "response_format" not in model_input:
+            return None
+        response_format = model_input["response_format"]
+        if "type" not in response_format or response_format["type"] != "json_schema":
+            raise HTTPException(
+                status_code=400,
+                detail='response_format["type"] must be json_schema.',
+            )
+        if "json_schema" not in response_format:
+            raise HTTPException(
+                status_code=400,
+                detail='response_format["json_schema"] must be provided.',
+            )
+        json_schema = response_format["json_schema"]
+        if "schema" not in json_schema:
+            raise HTTPException(
+                status_code=400,
+                detail='response_format["json_schema"]["schema"] must be provided.',
+            )
+        return json_schema["schema"]
 
 
 def set_briton_request_fields_from_model_input(model_input, briton_request):
