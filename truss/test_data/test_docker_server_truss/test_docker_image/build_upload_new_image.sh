83b02a5fb8b32c4f49c7529be0a6b25dd8c3a3f9
Tianshu Cheng <tianshuc@Tianshus-MBP.MG8702>
2024-09-25 17:09:58 -0700

    build amd64 image

--- truss/test_data/test_docker_server_truss/test_docker_image/build_upload_new_image.sh
+++ truss/test_data/test_docker_server_truss/test_docker_image/build_upload_new_image.sh
@@ -1,6 +1,6 @@
 # Read the version from the VERSION file
 VERSION=$(cat VERSION)
 
-docker build -t fastapi-test .
-docker tag fastapi-test baseten/fastapi-test:$VERSION
-docker push baseten/fastapi-test:$VERSION
+docker buildx build --platform linux/amd64,linux/arm64 -f ./Dockerfile -t fastapi-test:$VERSION . || { echo "Failed to build the Docker image"; exit 1; }
+docker tag fastapi-test:$VERSION baseten/fastapi-test:$VERSION || { echo "Failed to tag the Docker image."; exit 1; }
+docker push baseten/fastapi-test:$VERSION || { echo "Failed to push the Docker image."; exit 1; }
