336fd95dc19bc4bea3d1d002f5dc7d5d919abc62
samiksha <20553087+spal1@users.noreply.github.com>
2024-10-15 18:41:40 +0000

    call setup env before load

--- truss/local/local_config_handler.py
+++ truss/local/local_config_handler.py
@@ -77,11 +77,15 @@ class LocalConfigHandler:
 
     @staticmethod
     def dynamic_config_path():
-        dynamic_config_dir = (
-            LocalConfigHandler.TRUSS_CONFIG_DIR / "etc/b10_dynamic_config"
-        )
+        dynamic_config_dir = LocalConfigHandler.TRUSS_CONFIG_DIR / "b10_dynamic_config"
         dynamic_config_dir.mkdir(exist_ok=True, parents=True)
         return dynamic_config_dir
+
+    @staticmethod
+    def set_dynamic_config(key: str, value: str):
+        key_path = LocalConfigHandler.dynamic_config_path() / key
+        with key_path.open("w") as key_file:
+            key_file.write(value)
 
     @staticmethod
     def _signatures_dir_path():
