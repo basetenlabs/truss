f544e05be160524f4e5c003e231800ecb57bcdec
Sidharth Shanker <sshanker220@gmail.com>
2024-10-30 17:49:32 -0400

    Use aiohttp for chainlet <-> chainlet communication (#1194)
    * Switch to aiohttp for chains.
    
    * Minor refactor + better docstring.
    
    * Updated deps.
    
    * Fix types.
    
    * Fix poetry lock issue.
    
    * Revert boto3 changes.

--- truss-chains/truss_chains/stub.py
+++ truss-chains/truss_chains/stub.py
@@ -6,10 +6,14 @@ import threading
 import time
 from typing import Any, ClassVar, Mapping, Optional, Type, TypeVar, final
 
+import aiohttp
 import httpx
 import tenacity
 
 from truss_chains import definitions, utils
+
+DEFAULT_MAX_CONNECTIONS = 1000
+DEFAULT_MAX_KEEPALIVE_CONNECTIONS = 400
 
 
 class BasetenSession:
@@ -17,12 +21,13 @@ class BasetenSession:
 
     _client_cycle_time_sec: ClassVar[int] = 3600 * 1  # 1 hour.
     _client_limits: ClassVar[httpx.Limits] = httpx.Limits(
-        max_connections=1000, max_keepalive_connections=400
+        max_connections=DEFAULT_MAX_CONNECTIONS,
+        max_keepalive_connections=DEFAULT_MAX_KEEPALIVE_CONNECTIONS,
     )
     _auth_header: Mapping[str, str]
     _service_descriptor: definitions.ServiceDescriptor
     _cached_sync_client: Optional[tuple[httpx.Client, int]]
-    _cached_async_client: Optional[tuple[httpx.AsyncClient, int]]
+    _cached_async_client: Optional[tuple[aiohttp.ClientSession, int]]
 
     def __init__(
         self,
@@ -81,17 +86,22 @@ class BasetenSession:
         assert self._cached_sync_client is not None
         return self._cached_sync_client[0]
 
-    async def _client_async(self) -> httpx.AsyncClient:
+    async def _client_async(self) -> aiohttp.ClientSession:
         # Check `_client_cycle_needed` before and after locking to avoid
         # needing a lock each time the client is accessed.
         if self._client_cycle_needed(self._cached_async_client):
             async with self._async_lock:
                 if self._client_cycle_needed(self._cached_async_client):
+                    connector = aiohttp.TCPConnector(
+                        limit=DEFAULT_MAX_CONNECTIONS,
+                    )
                     self._cached_async_client = (
-                        httpx.AsyncClient(
+                        aiohttp.ClientSession(
                             headers=self._auth_header,
-                            timeout=self._service_descriptor.options.timeout_sec,
-                            limits=self._client_limits,
+                            connector=connector,
+                            timeout=aiohttp.ClientTimeout(
+                                total=self._service_descriptor.options.timeout_sec
+                            ),
                         ),
                         int(time.time()),
                     )
@@ -138,7 +148,7 @@ class BasetenSession:
                         resp = await client.post(
                             self._service_descriptor.predict_url, json=json_payload
                         )
-                    return utils.handle_response(resp, self.name)
+                    return await utils.handle_async_response(resp, self.name)
                 # As a special case we invalidate the client in case of certificate
                 # errors. This has happened in the past and is a defensive measure.
                 except ssl.SSLError:
