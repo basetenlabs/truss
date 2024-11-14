7b89ce91dc77aa8c098cad489864a136b775d995
Sidharth Shanker <sshanker220@gmail.com>
2024-11-14 11:36:45 -0500

    Ignore local aws config (#1230)
    * Ignore local aws config.
    
    * Ignore local aws config.
    
    * Temporarily override env vars.
    
    * remove cli override.
    
    * Pr feedback.
    
    * Revert change.

--- truss/remote/baseten/utils/transfer.py
+++ truss/remote/baseten/utils/transfer.py
@@ -5,6 +5,7 @@ import os
 import boto3
 from boto3.s3.transfer import TransferConfig
 from rich.progress import Progress
+from truss.util.env_vars import override_env_vars
 
 
 def base64_encoded_json_str(obj):
@@ -12,26 +13,29 @@ def base64_encoded_json_str(obj):
 
 
 def multipart_upload_boto3(file_path, bucket_name, key, credentials):
-    s3_resource = boto3.resource("s3", **credentials)
-    filesize = os.stat(file_path).st_size
+    # In the CLI flow, ignore any local ~/.aws/config files,
+    # which can interfere with uploading the Truss to S3.
+    with override_env_vars({"AWS_CONFIG_FILE": ""}):
+        s3_resource = boto3.resource("s3", **credentials)
+        filesize = os.stat(file_path).st_size
 
-    # Create a new progress bar
-    progress = Progress()
+        # Create a new progress bar
+        progress = Progress()
 
-    # Add a new task to the progress bar
-    task_id = progress.add_task("[cyan]Uploading...", total=filesize)
+        # Add a new task to the progress bar
+        task_id = progress.add_task("[cyan]Uploading...", total=filesize)
 
-    with progress:
+        with progress:
 
-        def callback(bytes_transferred):
-            # Update the progress bar
-            progress.update(task_id, advance=bytes_transferred)
+            def callback(bytes_transferred):
+                # Update the progress bar
+                progress.update(task_id, advance=bytes_transferred)
 
-        s3_resource.Object(bucket_name, key).upload_file(
-            file_path,
-            Config=TransferConfig(
-                max_concurrency=10,
-                use_threads=True,
-            ),
-            Callback=callback,
-        )
+            s3_resource.Object(bucket_name, key).upload_file(
+                file_path,
+                Config=TransferConfig(
+                    max_concurrency=10,
+                    use_threads=True,
+                ),
+                Callback=callback,
+            )
