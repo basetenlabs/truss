e61c2f01c0309172623d61cf787c59f7c747742b
joostinyi <63941848+joostinyi@users.noreply.github.com>
2024-12-16 11:12:43 -0800

    bump briton package to 0.3.13.dev3 (#1286)
    * bump briton package to 0.3.13.dev3 and truss version to 0.9.57
    
    * add default runtime
    
    * 0.9.57rc0
    
    * fix existing runtime case
    
    * 0.9.57rc1
    
    * use briton==0.3.13.dev4

--- truss/tests/trt_llm/test_trt_llm_config.py
+++ truss/tests/trt_llm/test_trt_llm_config.py
@@ -20,5 +20,20 @@ def test_trt_llm_configuration_init_and_migrate_deprecated_runtime_fields(
         "enable_chunked_context": True,
         "batch_scheduler_policy": TrussTRTLLMBatchSchedulerPolicy.MAX_UTILIZATION.value,
         "request_default_max_tokens": 10,
+        "total_token_limit": 50,
+    }
+
+
+def test_trt_llm_configuration_init_and_migrate_deprecated_runtime_fields_existing_runtime(
+    deprecated_trtllm_config_with_runtime_existing,
+):
+    trt_llm_config = TRTLLMConfiguration(
+        **deprecated_trtllm_config_with_runtime_existing["trt_llm"]
+    )
+    assert trt_llm_config.runtime.model_dump() == {
+        "kv_cache_free_gpu_mem_fraction": 0.1,
+        "enable_chunked_context": True,
+        "batch_scheduler_policy": TrussTRTLLMBatchSchedulerPolicy.MAX_UTILIZATION.value,
+        "request_default_max_tokens": 10,
         "total_token_limit": 100,
     }
