fedfc76492b1a7d8badbb06c065e46a54f716a9c
samiksha <20553087+spal1@users.noreply.github.com>
2024-12-20 22:24:10 +0000

    is_ready fail fast on load failure

--- truss/templates/server/model_wrapper.py
+++ truss/templates/server/model_wrapper.py
@@ -543,7 +543,7 @@ class ModelWrapper:
     async def is_ready(self) -> Optional[bool]:
         descriptor = self.model_descriptor.is_ready
         is_ready: Optional[bool] = None
-        if not descriptor:
+        if not descriptor or self.load_failed:
             return is_ready
         try:
             if descriptor.is_async:
