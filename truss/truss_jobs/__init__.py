e57aa3805b7f14f6d8701d0a98e93b6e031f550d
rcano-baseten <raymond.cano@baseten.co>
2025-02-14 16:58:55 +0000

    export typing

--- truss/truss_jobs/__init__.py
+++ truss/truss_jobs/__init__.py
@@ -1,3 +1,3 @@
-from truss.truss_jobs.definitions import ImageSpec, Secret
+from truss.truss_jobs.definitions import CustomImage, DockerImage, ImageSpec, Secret
 
-__all__ = ["ImageSpec", "Secret"]
+__all__ = ["ImageSpec", "Secret", "DockerImage", "CustomImage"]
