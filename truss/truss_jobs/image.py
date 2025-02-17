4c5a08659fe28e49f373f5987c80e23b4a25a2e4
rcano-baseten <raymond.cano@baseten.co>
2025-02-17 18:22:53 +0000

    secrets is not necessary

--- truss/truss_jobs/image.py
+++ truss/truss_jobs/image.py
@@ -114,5 +114,5 @@ def build_image_request(
             "pip_requirements": pip_requirements,
             "file_bundles": file_bundles,
         },
-        "build_secrets": [image_spec.build_secrets],
+        "build_secrets": image_spec.build_secrets,
     }
