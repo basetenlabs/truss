fd467128c3c839d5a5ab9b6e5b9503911ddeca19
rcano-baseten <raymond.cano@baseten.co>
2025-02-19 16:24:53 +0000

    adds file bundle

--- truss/truss_train/definitions.py
+++ truss/truss_train/definitions.py
@@ -1,1 +1,14 @@
 # Training types here
+import pathlib
+from typing import Union
+
+from truss.base.pydantic_models import SafeModelNonSerializable
+
+LocalPath = Union[str, pathlib.Path]
+
+
+class FileBundle(SafeModelNonSerializable):
+    """A bundle of files to be copied into the docker image."""
+
+    source_path: LocalPath
+    remote_path: str
