c0c066f5da2a5c1532149ae0c73ac801ad63c34d
rcano-baseten <raymond.cano@baseten.co>
2025-02-19 16:42:53 +0000

    adds EnvironmentVariables type

--- truss/truss_train/definitions.py
+++ truss/truss_train/definitions.py
@@ -1,8 +1,8 @@
 # Training types here
 import pathlib
-from typing import Union
+from typing import Dict, Union
 
-from truss.base.pydantic_models import SafeModelNonSerializable
+from truss.base.pydantic_models import SafeModel, SafeModelNonSerializable
 
 LocalPath = Union[str, pathlib.Path]
 
@@ -12,3 +12,22 @@ class FileBundle(SafeModelNonSerializable):
 
     source_path: LocalPath
     remote_path: str
+
+
+class SecretReference(SafeModel):
+    name: str
+
+    def dict(self, **kwargs):
+        return {"name": self.name, "type": "secret"}
+
+
+class EnvironmentVariables(SafeModelNonSerializable):
+    """A dictionary for environment variables that can contain strings or secret references."""
+
+    __root__: Dict[str, Union[str, SecretReference]]
+
+    def dict(self):
+        return {
+            k: v.dict() if isinstance(v, SecretReference) else v
+            for k, v in self.__root__.items()
+        }
