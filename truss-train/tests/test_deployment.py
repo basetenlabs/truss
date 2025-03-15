549874f6a65d51ecd585f3d53fc9f1e5f35331ee
Raymond Cano <raymond.cano@baseten.co>
2025-03-14 22:59:24 -0400

    fixes initialization for nested objs

--- truss-train/tests/test_deployment.py
+++ truss-train/tests/test_deployment.py
@@ -1,8 +1,9 @@
 import pathlib
 from unittest import mock
 
+from truss.base import truss_config
 from truss_train import deployment
-from truss_train.definitions import Image, TrainingJob
+from truss_train.definitions import Image, TrainingJob, Compute
 
 
 @mock.patch("truss.remote.baseten.utils.transfer.multipart_upload_boto3")
@@ -18,7 +19,11 @@ def test_prepare_push(get_blob_credentials_mock, multipart_upload_boto3_mock):
     prepared_job = deployment.prepare_push(
         mock_api,
         pathlib.Path(__file__),
-        TrainingJob(image=Image(base_image="hello-world")),
+        TrainingJob(image=Image(base_image="hello-world"), compute=Compute(
+            node_count=1,
+            accelerator=truss_config.AcceleratorSpec(
+                accelerator=truss_config.Accelerator.T4.value,
+        ))),
     )
     assert len(prepared_job.runtime_artifacts) == 1
     assert prepared_job.runtime_artifacts[0].s3_key == "test-s3-key"
