8e9724718f82d6435f2c74f881865aab0c0546f8
michaelfeil <63565275+michaelfeil@users.noreply.github.com>
2025-03-18 17:05:39 +0000

    add config check for encoder

--- truss/trt_llm/config_checks.py
+++ truss/trt_llm/config_checks.py
@@ -51,12 +51,12 @@ model_metadata:
 
     if uses_trt_llm_builder(tr):
         assert tr.spec.config.trt_llm is not None
+        if tr.spec.config.trt_llm.build.base_model == TrussTRTLLMModel.ENCODER:
+            return ""
+        # only briton requires openai-compatible tag, all others don't care about the openai tag
         current_tags = tr.spec.config.model_metadata.get("tags", [])
 
-        if (
-            tr.spec.config.trt_llm.build.speculator is not None
-            and tr.spec.config.trt_llm.build.base_model != TrussTRTLLMModel.ENCODER
-        ):
+        if tr.spec.config.trt_llm.build.speculator is not None:
             # spec-dec has no classic backend. OpenAI-mode is forced, regardless of tags.
             if OPENAI_NON_COMPATIBLE_TAG in current_tags:
                 return (
