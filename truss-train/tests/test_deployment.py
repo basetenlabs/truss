3a16eeb5b79d20059dcc881733bf0826d868fa34
Raymond Cano <raymond.cano@baseten.co>
2025-04-01 13:47:50 -0400

    adds test

--- truss-train/tests/test_deployment.py
+++ truss-train/tests/test_deployment.py
@@ -5,11 +5,14 @@ import pytest
 
 from truss.base import truss_config
 from truss_train import deployment
-from truss_train.definitions import Compute, Image, TrainingJob
+from truss_train.definitions import Compute, Image, Runtime, TrainingJob
 
 
 @mock.patch("truss.remote.baseten.utils.transfer.multipart_upload_boto3")
 @mock.patch("truss.remote.baseten.api.BasetenApi.get_blob_credentials")
+@pytest.mark.parametrize(
+    "use_read_write_cache", [True, False]
+)
 @pytest.mark.parametrize(
     "compute",
     [
@@ -27,7 +30,7 @@ from truss_train.definitions import Compute, Image, TrainingJob
         ),
     ],
 )
-def test_prepare_push(get_blob_credentials_mock, multipart_upload_boto3_mock, compute):
+def test_prepare_push(get_blob_credentials_mock, multipart_upload_boto3_mock, compute, use_read_write_cache):
     mock_api = mock.Mock()
     mock_api.get_blob_credentials.return_value = {
         "s3_bucket": "test-s3-bucket",
@@ -38,7 +41,7 @@ def test_prepare_push(get_blob_credentials_mock, multipart_upload_boto3_mock, co
     prepared_job = deployment.prepare_push(
         mock_api,
         pathlib.Path(__file__),
-        TrainingJob(image=Image(base_image="hello-world"), compute=compute),
+        TrainingJob(image=Image(base_image="hello-world"), compute=compute, runtime=Runtime(use_read_write_cache=use_read_write_cache)),
     )
     assert len(prepared_job.runtime_artifacts) == 1
     assert prepared_job.runtime_artifacts[0].s3_key == "test-s3-key"
@@ -50,5 +53,6 @@ def test_prepare_push(get_blob_credentials_mock, multipart_upload_boto3_mock, co
         )
     else:
         assert prepared_job.compute.accelerator is None
+    assert prepared_job.runtime.use_read_write_cache == use_read_write_cache
     # ensure that serialization works
     prepared_job.model_dump()
