ecd5d768a6fe764f226d824b80dc098488a3f9e2
Raymond Cano <raymond.cano@baseten.co>
2025-04-01 10:42:18 -0400

    more pr review

--- truss/cli/train.py
+++ truss/cli/train.py
@@ -1,19 +1,22 @@
 from typing import Optional, Tuple, cast
-import rich_click as click
-import rich
 
-from truss.remote.baseten.remote import BasetenRemote
+import rich
+import rich_click as click
 from InquirerPy import inquirer
 from rich.console import Console
 
+from truss.remote.baseten.remote import BasetenRemote
 
-def get_args_for_stop(console: Console, remote_provider: BasetenRemote, project_id: Optional[str], job_id: Optional[str]) -> Tuple[str, str]:
 
+def get_args_for_stop(
+    console: Console,
+    remote_provider: BasetenRemote,
+    project_id: Optional[str],
+    job_id: Optional[str],
+) -> Tuple[str, str]:
     if not project_id or not job_id:
         # get all running jobs
         job = _get_running_job(console, remote_provider, project_id, job_id)
-        if not job:
-            raise click.UsageError("Unable to stop training job. No running jobs found.")
         project_id_for_job = cast(str, job["training_project_id"])
         job_id_to_stop = cast(str, job["id"])
         # check if the user wants to stop the inferred running job
@@ -29,7 +32,13 @@ def get_args_for_stop(console: Console, remote_provider: BasetenRemote, project_
 
     return project_id, job_id
 
-def get_args_for_logs(console: Console, remote_provider: BasetenRemote, project_id: Optional[str], job_id: Optional[str]) -> Tuple[str, str]:
+
+def get_args_for_logs(
+    console: Console,
+    remote_provider: BasetenRemote,
+    project_id: Optional[str],
+    job_id: Optional[str],
+) -> Tuple[str, str]:
     if not project_id or not job_id:
         jobs = remote_provider.api.search_training_jobs(
             project_id=project_id, job_id=job_id
@@ -37,31 +46,33 @@ def get_args_for_logs(console: Console, remote_provider: BasetenRemote, project_
         if not jobs:
             raise click.UsageError("Unable to get logs. No jobs found.")
         if len(jobs) > 1:
-            console.print("Multiple jobs found. Showing logs for the most recently created job.", style="yellow")
             sorted_jobs = sorted(jobs, key=lambda x: x["created_at"], reverse=True)
             job = sorted_jobs[0]
+            console.print(
+                f"Multiple jobs found. Showing logs for the most recently created job: {job['id']}",
+                style="yellow",
+            )
         else:
             job = jobs[0]
         project_id = cast(str, job["training_project_id"])
         job_id = cast(str, job["id"])
     return project_id, job_id
 
+
 def _get_running_job(
-    console: Console, remote_provider: BasetenRemote, project_id: Optional[str], job_id: Optional[str]
-):
+    console: Console,
+    remote_provider: BasetenRemote,
+    project_id: Optional[str],
+    job_id: Optional[str],
+) -> dict:
     jobs = remote_provider.api.search_training_jobs(
         statuses=["TRAINING_JOB_RUNNING"], project_id=project_id, job_id=job_id
     )
     if not jobs:
-        console.print("No running jobs found.", style="yellow")
-        return
+        raise click.UsageError("No running jobs found.")
     if len(jobs) > 1:
         display_training_jobs(console, jobs, title="Running Training Jobs")
-        console.print(
-            "Multiple running jobs found. Please specify a project and job id.",
-            style="yellow",
-        )
-        return
+        raise click.UsageError("Multiple running jobs found. Please specify a job id.")
     return jobs[0]
 
 
