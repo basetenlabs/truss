adeb0745adc82dc059bb76a98b575c94a690eaa5
Raymond Cano <raymond.cano@baseten.co>
2025-04-02 10:16:34 -0400

    truss repository

--- truss-train/tests/test_deployment.py
+++ truss-train/tests/test_deployment.py
@@ -10,7 +10,7 @@ from truss_train.definitions import Compute, Image, Runtime, TrainingJob
 
 @mock.patch("truss.remote.baseten.utils.transfer.multipart_upload_boto3")
 @mock.patch("truss.remote.baseten.api.BasetenApi.get_blob_credentials")
-@pytest.mark.parametrize("use_read_write_cache", [True, False])
+@pytest.mark.parametrize("enable_cache", [True, False])
 @pytest.mark.parametrize(
     "compute",
     [
@@ -32,7 +32,7 @@ def test_prepare_push(
     get_blob_credentials_mock,
     multipart_upload_boto3_mock,
     compute,
-    use_read_write_cache,
+    enable_cache,
 ):
     mock_api = mock.Mock()
     mock_api.get_blob_credentials.return_value = {
@@ -47,7 +47,7 @@ def test_prepare_push(
         TrainingJob(
             image=Image(base_image="hello-world"),
             compute=compute,
-            runtime=Runtime(use_read_write_cache=use_read_write_cache),
+            runtime=Runtime(enable_cache=enable_cache),
         ),
     )
     assert len(prepared_job.runtime_artifacts) == 1
@@ -60,6 +60,6 @@ def test_prepare_push(
         )
     else:
         assert prepared_job.compute.accelerator is None
-    assert prepared_job.runtime.use_read_write_cache == use_read_write_cache
+    assert prepared_job.runtime.enable_cache == enable_cache
     # ensure that serialization works
     prepared_job.model_dump()
