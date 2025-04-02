451d62c4333ad3f8b797465b4ed639acd7932e09
michaelfeil <63565275+michaelfeil@users.noreply.github.com>
2025-04-02 20:54:36 +0000

    add test

--- truss/tests/contexts/image_builder/test_serving_image_builder.py
+++ truss/tests/contexts/image_builder/test_serving_image_builder.py
@@ -284,7 +284,8 @@ def test_test_truss_server_model_cache_v2(test_data_path):
 def test_model_cache_dockerfile(test_data_path):
     truss_dir = test_data_path / "test_truss_server_model_cache_v1"
     tr = TrussHandle(truss_dir)
-
+    assert tr.spec.config.model_cache.is_v1
+    assert not tr.spec.config.model_cache.is_v2
     builder_context = ServingImageBuilderContext
     image_builder = builder_context.run(tr.spec.truss_dir)
 
@@ -300,6 +301,8 @@ def test_model_cache_dockerfile(test_data_path):
 def test_model_cache_dockerfile_v2(test_data_path):
     truss_dir = test_data_path / "test_truss_server_model_cache_v2"
     tr = TrussHandle(truss_dir)
+    assert not tr.spec.config.model_cache.is_v1
+    assert tr.spec.config.model_cache.is_v2
     # assert tr.spec.config.model_cache.is_v2
     builder_context = ServingImageBuilderContext
     image_builder = builder_context.run(tr.spec.truss_dir)
