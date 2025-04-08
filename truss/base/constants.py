bc9bd9c514801bbb723746fdeefc33c4ad616852
Bryce Dubayah <bdubayah@gmail.com>
2025-04-07 17:45:31 -0700

    bump versions (#1559)

--- truss/base/constants.py
+++ truss/base/constants.py
@@ -86,7 +86,7 @@ REGISTRY_BUILD_SECRET_PREFIX = "DOCKER_REGISTRY_"
 
 TRTLLM_SPEC_DEC_TARGET_MODEL_NAME = "target"
 TRTLLM_SPEC_DEC_DRAFT_MODEL_NAME = "draft"
-TRTLLM_BASE_IMAGE = "baseten/briton-server:v0.17.0-e152566"
+TRTLLM_BASE_IMAGE = "baseten/briton-server:v0.17.0-fd30ac1"
 # TODO: build the image so that the default path `python3` can be used - then remove here.
 TRTLLM_PYTHON_EXECUTABLE = "/usr/local/briton/venv/bin/python"
 BEI_TRTLLM_BASE_IMAGE = "baseten/bei:0.0.20"
