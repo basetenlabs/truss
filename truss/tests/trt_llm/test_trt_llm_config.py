3971bcd9cdc583770931461c54b9ca2bbdaa1ada
Phillippe Siclait <546332+siclait@users.noreply.github.com>
2025-04-09 23:47:51 +0000

    Update TRT-LLM config test

--- truss/tests/trt_llm/test_trt_llm_config.py
+++ truss/tests/trt_llm/test_trt_llm_config.py
@@ -49,6 +49,7 @@ def test_trt_llm_configuration_init_and_migrate_deprecated_runtime_fields(
         "batch_scheduler_policy": TrussTRTLLMBatchSchedulerPolicy.MAX_UTILIZATION.value,
         "request_default_max_tokens": 10,
         "total_token_limit": 50,
+        "served_model_name": None,
         "webserver_default_route": None,
     }
 
