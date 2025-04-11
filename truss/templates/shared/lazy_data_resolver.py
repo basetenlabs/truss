59ba09b5fcb5570e311c1ff863008b068d1f3ef6
michaelfeil <63565275+michaelfeil@users.noreply.github.com>
2025-04-11 00:56:49 +0000

    background prefetch

--- truss/templates/shared/lazy_data_resolver.py
+++ truss/templates/shared/lazy_data_resolver.py
@@ -1,18 +1,37 @@
 from functools import lru_cache
 from pathlib import Path
+from threading import Lock, Thread
 
 LAZY_DATA_RESOLVER_PATH = Path("/bptr/bptr-manifest")
 
 
 class LazyDataResolverV2:
+    """Lazy data resolver pre-fetches data in a separate thread.
+    It uses a lock to ensure that the data is only fetched once
+    and that the thread is not blocked by other threads.
+    """
+
     def __init__(self, data_dir: Path):
         self._data_dir = data_dir
-        self.fetch()
+        self._lock = Lock()
+        Thread(target=self._prefetch_thread, daemon=True).start()
+
+    def _prefetch_thread(self):
+        self._fetch()
 
     @lru_cache
-    def fetch(self) -> str:
+    def _fetch(self) -> str:
         if not LAZY_DATA_RESOLVER_PATH.is_file():
             return ""
         import truss_transfer
 
         return truss_transfer.lazy_data_resolve(str(self._data_dir))
+
+    def block_until_fetched(self):
+        """blocks until the data has been downloaded."""
+        with self._lock:
+            return self._fetch()
+
+    def fetch(self) -> str:
+        """deprecated: use block_until_fetched instead"""
+        return self.block_until_fetched()
