a4cd7554d4d579d589e7127552c3de171e44118f
michaelfeil <63565275+michaelfeil@users.noreply.github.com>
2025-05-13 20:03:41 +0000

    remove config checks

--- truss/tests/util/test_config_checks.py
+++ truss/tests/util/test_config_checks.py
@@ -1,22 +1,8 @@
-from unittest.mock import patch
-
-import pytest
-
 from truss.base.constants import TRTLLM_MIN_MEMORY_REQUEST_GI
 from truss.trt_llm.config_checks import memory_updated_for_trt_llm_builder
 from truss.truss_handle.truss_handle import TrussHandle
 
 
-@patch("truss.trt_llm.config_checks._is_model_public")
-@pytest.mark.parametrize(
-    "has_secret, is_model_public, expected_result",
-    [
-        (False, False, True),
-        (False, True, False),
-        (True, False, False),
-        (True, True, False),
-    ],
-)
 def test_check_and_update_memory_for_trt_llm_builder(custom_model_trt_llm):
     handle = TrussHandle(custom_model_trt_llm)
     assert memory_updated_for_trt_llm_builder(handle)
