ec1ce4cf8d03103ea88f96452743b8c5a1463098
michaelfeil <63565275+michaelfeil@users.noreply.github.com>
2025-05-24 22:06:03 +0000

    ci and bindings

--- baseten-inference-client/tests/test_bindings.py
+++ baseten-inference-client/tests/test_bindings.py
@@ -2,6 +2,10 @@ def test_baseten_inference_client_bindings_basic_test():
     from baseten_inference_client import InferenceClient
 
     InferenceClient.embed
+    InferenceClient.aembed
     InferenceClient.rerank
+    InferenceClient.arerank
     InferenceClient.classify
+    InferenceClient.aclassify
     InferenceClient.batch_post
+    InferenceClient.abatch_post
