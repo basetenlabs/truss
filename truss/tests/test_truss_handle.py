2c52c3bffff0214710b9bf1f452edc24983b72f5
Nikhil Narayen <nikhil.narayen@gmail.com>
2025-06-05 12:38:20 -0500

    Use newer image for integration tests (#1697)

--- truss/tests/test_truss_handle.py
+++ truss/tests/test_truss_handle.py
@@ -104,7 +104,11 @@ def test_build_docker_image(custom_model_truss_dir_with_pre_and_post):
 @pytest.mark.parametrize(
     "base_image, path, expected_fail",
     [
-        ("baseten/truss-server-base:3.9-v0.4.8rc4", "/usr/local/bin/python3", False),
+        ("baseten/truss-server-base:3.9-v0.9.0", "/usr/local/bin/python3", False),
+        ("baseten/truss-server-base:3.10-v0.9.0", "/usr/local/bin/python3", False),
+        ("baseten/truss-server-base:3.11-v0.9.0", "/usr/local/bin/python3", False),
+        ("baseten/truss-server-base:3.12-v0.9.0", "/usr/local/bin/python3", False),
+        ("baseten/truss-server-base:3.13-v0.9.0", "/usr/local/bin/python3", False),
         ("python:3.8", "/usr/local/bin/python3", False),
         ("python:3.10", "/usr/local/bin/python3", False),
         ("python:3.11", "/usr/local/bin/python3", False),
