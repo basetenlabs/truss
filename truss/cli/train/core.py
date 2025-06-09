ebf0b7b8462cd6c2bbf10d2d80b773c6a7d59d6a
Aghilan Nathan <90127404+aghilann@users.noreply.github.com>
2025-06-09 15:23:28 +0000

    fix(truss): lint

--- truss/cli/train/core.py
+++ truss/cli/train/core.py
@@ -259,7 +259,7 @@ def display_training_job(console: Console, job: dict, checkpoints: list[dict] = 
 
 
 def download_training_job_data(
-    remote_provider: BasetenRemote, job_id: str, output_dir: Optional[Path] = None
+    remote_provider: BasetenRemote, job_id: str, output_dir: Path = None
 ) -> Path:
     jobs = remote_provider.api.search_training_jobs(job_id=job_id)
 
