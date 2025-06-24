5e0c14cd1804d946811a445b13b8af976eade27a
Raymond Cano <raymond.cano@baseten.co>
2025-06-23 18:23:09 -0700

    training job docs

--- truss-train/truss_train/public_api.py
+++ truss-train/truss_train/public_api.py
@@ -8,6 +8,17 @@ from truss_train.deployment import create_training_job
 
 
 def push(training_project: TrainingProject, config: Path, remote: str = "baseten"):
+    """
+    push
+    * creates or updates a training_project
+    * creates the training_job in the training_project
+    * returns a dictionary with the format:
+        {
+            "training_project": TrainingProject,
+            "training_job": TrainingJob,
+        }. The definitions of TrainingProject and TrainingJob can be found in the API docs.
+        https://docs.baseten.co/reference/training-api/get-training-job
+    """
     remote_provider: BasetenRemote = cast(
         BasetenRemote, RemoteFactory.create(remote=remote)
     )
