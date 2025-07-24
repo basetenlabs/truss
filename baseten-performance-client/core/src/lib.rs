ceda26f48a3b944d50378c607fe755d92a533c05
michaelfeil <me@michaelfeil.eu>
2025-07-24 23:16:11 +0000

    minor refactor

--- baseten-performance-client/core/src/lib.rs
+++ baseten-performance-client/core/src/lib.rs
@@ -2,6 +2,7 @@ pub mod client;
 pub mod constants;
 pub mod errors;
 pub mod http;
+pub mod http_client;
 pub mod split_policy;
 pub mod utils;
 
@@ -9,5 +10,6 @@ pub use client::PerformanceClientCore;
 pub use constants::*;
 pub use errors::ClientError;
 pub use http::*;
+pub use http_client::*;
 pub use split_policy::*;
 pub use utils::*;
