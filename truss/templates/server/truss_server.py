473eb6b98ba1e2bb7991402184ef0bca6a7768fd
Raymond Cano <raymond.cano@baseten.co>
2025-07-30 14:44:59 -0400

    change liveness

--- truss/templates/server/truss_server.py
+++ truss/templates/server/truss_server.py
@@ -390,7 +390,7 @@ class TrussServer:
             on_startup=[self.on_startup],
             routes=[
                 # liveness endpoint
-                FastAPIRoute(r"/", lambda: True),
+                FastAPIRoute(r"/", self._endpoints.model_ready),
                 # readiness endpoint
                 FastAPIRoute(
                     r"/v1/models/{model_name}", self._endpoints.model_ready, tags=["V1"]
