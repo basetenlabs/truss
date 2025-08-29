9c3a0af4ed6dbaeb83edddbda8eea2bcbcf5ae9a
michaelfeil <me@michaelfeil.eu>
2025-08-29 20:24:54 +0000

    bump transfer

--- truss-transfer/src/create/object_storage_client.rs
+++ truss-transfer/src/create/object_storage_client.rs
@@ -1,5 +1,19 @@
 use object_store::client::{ClientConfigKey, ClientOptions};
 
 pub fn get_client_options() -> ClientOptions {
+    for var in &[
+        "HTTP_PROXY",
+        "http_proxy",
+        "HTTPS_PROXY",
+        "https_proxy",
+        "ALL_PROXY",
+        "all_proxy",
+    ] {
+        std::env::remove_var(var);
+    }
+    // In case libraries read NO_PROXY
+    std::env::set_var("NO_PROXY", "*");
+    std::env::set_var("no_proxy", "*");
+
     ClientOptions::new().with_config(ClientConfigKey::ProxyExcludes, "*")
 }
