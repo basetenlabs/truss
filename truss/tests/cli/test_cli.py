73d1ada8dca3006856d0e8dc6d249737da9c3fa0
Sidharth Shanker <sid.shanker@baseten.co>
2026-01-26 18:55:27 -0500

    Revert "print warnings if --watch not submitted for development deployment, k…"
    This reverts commit fbb58589220022a94c3564492a9134316de177aa.

--- truss/tests/cli/test_cli.py
+++ truss/tests/cli/test_cli.py
@@ -11,20 +11,11 @@ def test_push_with_grpc_transport_fails_for_development_deployment():
 
     runner = CliRunner()
 
-    # Test that gRPC transport fails with --watch (development deployment)
     with patch("truss.cli.cli._get_truss_from_directory", return_value=mock_truss):
         with patch("truss.cli.remote_cli.inquire_remote_name", return_value="remote1"):
             result = runner.invoke(
                 truss_cli,
-                [
-                    "push",
-                    "test_truss",
-                    "--remote",
-                    "remote1",
-                    "--model-name",
-                    "name",
-                    "--watch",
-                ],
+                ["push", "test_truss", "--remote", "remote1", "--model-name", "name"],
             )
 
     assert result.exit_code == 2
