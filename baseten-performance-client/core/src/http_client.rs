79e8e371ae1f2cbac47fd1be8001398b0e224f33
Michael Feil <63565275+michaelfeil@users.noreply.github.com>
2026-01-27 13:37:09 -0800

    revert tests

--- baseten-performance-client/core/src/http_client.rs
+++ baseten-performance-client/core/src/http_client.rs
@@ -103,17 +103,14 @@ where
         );
     }
 
-    let response_json_value: serde_json::Value =
-        if method.has_body() || matches!(method, crate::http::HttpMethod::GET) {
-            successful_response
-                .json::<serde_json::Value>()
-                .await
-                .map_err(|e| {
-                    ClientError::Serialization(format!("Failed to parse response JSON: {}", e))
-                })?
-        } else {
-            serde_json::Value::Object(serde_json::Map::new())
-        };
+    let response_json_value: serde_json::Value = if method.has_body() || matches!(method, crate::http::HttpMethod::GET) {
+        successful_response
+            .json::<serde_json::Value>()
+            .await
+            .map_err(|e| ClientError::Serialization(format!("Failed to parse response JSON: {}", e)))?
+    } else {
+        serde_json::Value::Object(serde_json::Map::new())
+    };
 
     Ok((response_json_value, headers_map))
 }
