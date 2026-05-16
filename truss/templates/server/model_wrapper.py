9a7e8c5ba50f949d59f720ffe8d494c51475ea71
Dev-X25874 <283057883+Dev-X25874@users.noreply.github.com>
2026-05-16 11:56:00 +0530

    server/model_wrapper: fix ArgConfig.prepare_args silently skipping NONE branch and raising NotImplementedError

--- truss/templates/server/model_wrapper.py
+++ truss/templates/server/model_wrapper.py
@@ -192,7 +192,7 @@ class ArgConfig(enum.Enum):
         args: _ArgsType
         if descriptor.arg_config == ArgConfig.NONE:
             args = ()
-        if descriptor.arg_config == ArgConfig.INPUTS_ONLY:
+        elif descriptor.arg_config == ArgConfig.INPUTS_ONLY:
             args = (inputs,)
         elif descriptor.arg_config == ArgConfig.REQUEST_ONLY:
             args = (request,)
@@ -201,7 +201,6 @@ class ArgConfig(enum.Enum):
         else:
             raise NotImplementedError(f"Arg config {descriptor.arg_config}.")
         return args
-
 
 @dataclasses.dataclass
 class MethodDescriptor:
